5414307c996a1ee79e40d91977f8129b2c4c9c3f
thanhtran-git <thanh.tran@live.de>
2025-01-17 09:19:52 +0100

    rename functions

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,9 +1,9 @@
-export const isValidUrl = (input: string): boolean => {
+export function isValidUrl(input: string): boolean {
     const regex = /^(https?:\/\/)?([\w\d-]+\.)+[a-z]{2,6}(\/[\w\d-]+)*\/?$/i;
     return regex.test(input);
   };
   
-  export const generateShortId = (): string => {
+  export function generateShortId(): string {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < 6; i++) {
