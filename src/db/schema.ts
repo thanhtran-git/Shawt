57b2e06eb09a79644fb3147e7f78cf572dde3e3e
thanhtran-git <thanh.tran@live.de>
2025-01-23 17:47:14 +0000

    update table name

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
 import { pgTable, integer, varchar, timestamp } from "drizzle-orm/pg-core";
 
-export const urls = pgTable("urls", {
+export const urlsTable = pgTable("urls", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  shortUrl: varchar("short_url", { length: 255 }).notNull(),
   longUrl: varchar("long_url", { length: 2048 }).notNull(),
+  shortUrl: varchar("short_url", { length: 255 }).notNull(),  
   createdAt: timestamp("created_at").defaultNow(),
 });
