cf59fb08ad1a67bafe6d6b475273cf112297edaf
thanhtran-git <thanh.tran@live.de>
2025-01-24 08:58:52 +0000

    add console.log error

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from 'next/server';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { urlsTable } from '@/db/schema';
 
-const db = drizzle(process.env.DATABASE_URL!);
+const db = drizzle(process.env.POSTGRES_URL!);
 
 export async function GET() {
   try {
     const messages = await db.select().from(urlsTable);
     return NextResponse.json(messages);
   } catch (error) {
+    console.error('Error in GET:', error); // Log the error
     return NextResponse.json({ error: 'Failed to fetch messages' }, { status: 500 });
   }
 }
@@ -16,10 +17,11 @@ export async function GET() {
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    console.log('Request body:', body)
+    console.log('Request body:', body);
     await db.insert(urlsTable).values(body);
     return NextResponse.json({ message: 'Message created successfully' });
   } catch (error) {
+    console.error('Error in POST:', error); // Log the error
     return NextResponse.json({ error: 'Failed to create message' }, { status: 500 });
   }
 }
