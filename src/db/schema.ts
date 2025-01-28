ee8d59b5abceb243c14171d9111fa8eed422174b
thanhtran-git <thanh.tran@live.de>
2025-01-28 08:51:54 +0000

    add short id column

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,6 +3,7 @@ import { pgTable, integer, varchar, timestamp } from "drizzle-orm/pg-core";
 export const urlsTable = pgTable("urls", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   longUrl: varchar("long_url", { length: 2048 }).notNull(),
-  shortUrl: varchar("short_url", { length: 255 }).notNull(),  
+  shortUrl: varchar("short_url", { length: 255 }).notNull(),
+  shortId: varchar("short_id", { length: 255 }).notNull().unique(),
   createdAt: timestamp("created_at").defaultNow(),
 });
