ed0afaec4952f03cdc9cb58be995b31dfdf2298d
thanhtran-git <thanh.tran@live.de>
2025-01-28 08:51:12 +0000

    delete old migration

--- drizzle/0000_slimy_payback.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "urls" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "urls_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"long_url" varchar(2048) NOT NULL,
-	"short_url" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now()
-);
