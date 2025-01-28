45268ec6a5ef3e2395a44b903d9f8bb5fd3ae7f3
thanhtran-git <thanh.tran@live.de>
2025-01-28 08:52:08 +0000

    add dynamic route

---
+++ src/app/[shortId]/page.tsx
@@ -0,0 +1,33 @@
+import { redirect } from "next/navigation";
+import { urlsTable } from "@/db/schema";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { eq } from "drizzle-orm";
+
+const db = drizzle(process.env.POSTGRES_URL!);
+
+interface RedirectPageProps {
+  params: { shortId: string };
+}
+
+export default async function RedirectPage({ params }: RedirectPageProps) {
+  const { shortId } = params;
+  let destination: string | undefined
+  
+  try {
+    const [urlEntry] = await db
+      .select()      
+      .from(urlsTable)
+      .where(eq(urlsTable.shortId, String(shortId)))
+      .limit(1);
+      
+    destination = urlEntry?.longUrl; 
+  } catch (error) {
+    console.error(`No URL found for shortId: ${shortId}`);
+    redirect("/404")
+  }
+
+  if (!destination) {
+    return <div>404 URL Not Found.</div>
+  }
+  redirect(destination);
+}
