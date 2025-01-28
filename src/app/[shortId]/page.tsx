1925555ba84613d43de6135fc0a8dac1bbde9704
thanhtran-git <thanh.tran@live.de>
2025-01-28 13:32:55 +0000

    update params type to Promise

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -5,19 +5,17 @@ import { eq } from "drizzle-orm";
 
 const db = drizzle(process.env.POSTGRES_URL!);
 
-interface RedirectPageProps {
-  params: { shortId: string };
-}
+type paramsType = Promise<{ shortId: string }>
 
-export default async function RedirectPage({ params }: RedirectPageProps) {
-  const { shortId } = params;
+export default async function RedirectPage(props: { params : paramsType}) {
+  const { shortId } = await props.params;
   let destination: string | undefined
   
   try {
     const [urlEntry] = await db
       .select()      
       .from(urlsTable)
-      .where(eq(urlsTable.shortId, String(shortId)))
+      .where(eq(urlsTable.shortId, shortId))
       .limit(1);
 
     destination = urlEntry?.longUrl; 
