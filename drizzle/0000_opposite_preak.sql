277354de847cbf4e4183f16849334cb22a98297b
thanhtran-git <thanh.tran@live.de>
2025-01-28 08:50:09 +0000

    update drizzle

---
+++ drizzle/0000_opposite_preak.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "urls" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "urls_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"long_url" varchar(2048) NOT NULL,
+	"short_url" varchar(255) NOT NULL,
+	"short_id" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
