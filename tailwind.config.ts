3b2736f77b299038f4676131f66c5366d5e093fd
thanhtran-git <thanh.tran@live.de>
2025-02-03 08:28:25 +0000

    revert paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
